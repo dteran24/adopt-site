6a48f4ddd83ec6b5a46fd8ce434180d605b24400
dteran24 <dteran21@yahoo.com>
2023-12-19 16:40:10 -0600

    remove ss in search

---
+++ src/app/components/pages/search.tsx
@@ -0,0 +1,240 @@
+"use client";
+import Items from "../../components/search/items";
+import Dropdown from "../../components/search/dropdownmenu";
+import combinedPetsData from "../../models/combinedPetsData.json";
+import { useEffect, useState } from "react";
+import ActiveFilter from "../../components/search/activefilters";
+import { Breed, FilterOptions, PetInfo, URLParameters } from "../../models/pet";
+import { getAnimals, getBreedList, getToken, upperCase } from "../../actions";
+import { useSearchParams, useRouter } from "next/navigation";
+import Loader from "../../components/loader";
+import { FaLessThan, FaGreaterThan } from "react-icons/fa";
+
+type searchProps = {
+  token: string;
+};
+
+const SearchComponent = (props: searchProps) => {
+  const { token } = props;
+  const typeParams = useSearchParams();
+  const router = useRouter();
+  const animal = typeParams.get("type");
+  const page = typeParams.get("page");
+  const location = typeParams.get("location");
+  const breedParam = typeParams.get("breed");
+
+  const defaultLocation = location !== undefined ? location : "";
+
+  const [type, setType] = useState(animal ? animal : "Dogs");
+  const [breedList, setBreedList] = useState<Breed[]>();
+  const [breed, setBreed] = useState(breedParam ? breedParam : "");
+  const [loading, setLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(page ? Number(page) : 1);
+  const [tokenData, setTokenData] = useState(token);
+  const [categoryValues, setCategoryValues] = useState<FilterOptions>({
+    breed: breed,
+    age: "",
+    size: "",
+    color: "",
+    gender: "",
+  });
+  const [parameters, setParameters] = useState<URLParameters>({
+    token: "",
+    filter: categoryValues,
+    type: type,
+    location: defaultLocation!,
+    page: pageNumber,
+  });
+  const [animals, setAnimals] = useState<PetInfo[]>();
+
+  const updateURL = (newType: string) => {
+    setType(newType);
+    router.replace(
+      `/search?type=${newType}&location=${location}&page=${pageNumber}&breed=${
+        categoryValues.breed === "Any" ? "" : categoryValues.breed
+      }`
+    );
+  };
+
+  const handleTypeChange = () => {
+    updateURL(type);
+  };
+
+  const fetchBreeds = async (token: string, type: string) => {
+    const response = await getBreedList(
+      token,
+      type.toLowerCase().replace("s", "")
+    );
+    if (response) {
+      setBreedList(response);
+    }
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+  
+    if (tokenData) {
+      setParameters({
+        token: tokenData,
+        filter: categoryValues,
+        type: type,
+        location: defaultLocation!,
+      });
+      setTokenData(tokenData);
+      fetchBreeds(tokenData, type);
+    } else {
+      const getTokenData = async () => {
+        try {
+          let data = await getToken();
+          if (data) {
+            setParameters({
+              token: data,
+              filter: categoryValues,
+              type: type,
+              location: defaultLocation!,
+            });
+            setTokenData(data);
+            fetchBreeds(data, type);
+          }
+        } catch (error) {
+          console.error("Error getting token:", error);
+        }
+      };
+
+      getTokenData();
+    }
+  }, [type]);
+  useEffect(() => {
+    const urlType = typeParams.get("type");
+    if (urlType != type && urlType) {
+      setType(urlType);
+    }
+  }, [typeParams, type, pageNumber]);
+
+  useEffect(() => {
+    handleTypeChange();
+    setParameters({
+      token: tokenData,
+      filter: categoryValues,
+      type: type,
+      page: pageNumber,
+      location: defaultLocation!,
+    });
+  }, [type, categoryValues, tokenData, pageNumber, location]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const animalsData = await getAnimals(
+        parameters.token,
+        parameters.filter,
+        parameters.type,
+        parameters.location,
+        parameters.page
+      );
+      if (animalsData) {
+        setAnimals(animalsData);
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [parameters]);
+
+  const handleDropdownChange = (category: string, value: string) => {
+    setCategoryValues({ ...categoryValues, [category]: value });
+  };
+  const setDefaultCategoryValues = () => {
+    setBreed("Any");
+    setCategoryValues((prev) => ({
+      ...prev,
+      breed: "Any",
+      age: "Any",
+      size: "Any",
+      color: "Any",
+      gender: "Any",
+    }));
+    setPageNumber(1);
+  };
+
+  let selectedData;
+  switch (type) {
+    case "Dogs":
+      selectedData = combinedPetsData.dogs;
+      break;
+    case "Cats":
+      selectedData = combinedPetsData.cats;
+      break;
+  }
+
+  return (
+    <main className="flex flex-col min-h-screen w-full bg-slate-100" id="items">
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <ActiveFilter
+            setAnimal={setType}
+            animal={type}
+            setDefaultCategoryValues={setDefaultCategoryValues}
+          />
+          <div className="flex flex-col">
+            <div className="flex justify-around">
+              <div className="w-72 rounded-lg bg-lime-500 m-5 mb-0 mx-auto sm:mx-5">
+                <ul className="px-5 pt-5 text-center">
+                  {selectedData?.map((filter, index) => (
+                    <li className="flex flex-col mb-5 lg:mb-20" key={index}>
+                      <span className="mb-3 text-lg font-bold">
+                        {upperCase(filter.title)}
+                      </span>
+                      <Dropdown
+                        breedList={breedList}
+                        items={filter.item}
+                        category={filter.title}
+                        handleDropdownChange={handleDropdownChange}
+                        animal={type}
+                        breed={breed}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div id="grid">
+                {animals ? (
+                  <>
+                    <Items parameters={parameters!} animals={animals} />
+                  </>
+                ) : (
+                  "..."
+                )}
+              </div>
+            </div>
+            <div className="text-black my-10 flex justify-center ms-72 gap-x-64">
+              {parameters.page && parameters.page <= 1 ? (
+                ""
+              ) : (
+                <a href="#grid">
+                  <button
+                    className="rounded bg-lime-400 p-2 hover:bg-lime-600 w-24"
+                    onClick={() => setPageNumber((prev) => prev - 1)}
+                  >
+                    <FaLessThan className="mx-auto text-lg" />
+                  </button>
+                </a>
+              )}
+              <a href="#grid">
+                <button
+                  className="rounded bg-lime-400 p-2 hover:bg-lime-600 w-24"
+                  onClick={() => setPageNumber((prev) => prev + 1)}
+                >
+                  <FaGreaterThan className="mx-auto text-lg" />
+                </button>
+              </a>
+            </div>
+          </div>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default SearchComponent;
