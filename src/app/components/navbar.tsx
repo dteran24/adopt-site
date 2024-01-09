a1437cc6118c696b575dfd9ec78b97c087c82ae5
dteran24 <dteran21@yahoo.com>
2024-01-09 14:31:40 -0600

    profile link in mobile

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -242,15 +242,33 @@ export default function Navbar() {
                 >
                   Contact
                 </Link>
+                {session && (
+                  <Link
+                    href="/account"
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Profile
+                  </Link>
+                )}
               </div>
               <div className="py-6">
-                {session ? <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" onClick={()=>logoutHandler()}>Log Out</button> : <Link
-                  href="/account/login"
-                  onClick={() => setMobileMenuOpen(false)}
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </Link>}
+                {session ? (
+                  <button
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    onClick={() => logoutHandler()}
+                  >
+                    Log Out
+                  </button>
+                ) : (
+                  <Link
+                    href="/account/login"
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </Link>
+                )}
               </div>
             </div>
           </div>
